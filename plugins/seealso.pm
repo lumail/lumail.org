319a9fe0e2c372a4c738cad013f6e26b506a1df4
Steve Kemp <steve@steve.org.uk>
2013-05-27 09:26:00 +0100

    New plugin to make "seealso" sections in our lua pages.

---
+++ plugins/seealso.pm
@@ -0,0 +1,67 @@
+#
+# This is a templer-plugin.
+#
+# Steve
+# --
+
+use strict;
+use warnings;
+
+
+package Templer::Plugin::seealso;
+
+sub new
+{
+    my ( $proto, %supplied ) = (@_);
+    my $class = ref($proto) || $proto;
+
+    my $self = {};
+    bless( $self, $class );
+    return $self;
+}
+
+
+sub expand_variables
+{
+    my ( $self, $site, $page, $data ) = (@_);
+
+    #
+    #  Get the page-variables in the template.
+    #
+    my %hash = %$data;
+
+    foreach my $key ( keys %hash )
+    {
+        if ( $key =~ /^seealso$/i )
+        {
+            #
+            #  For each function we'll add it to the list.
+            #
+            my $value = $hash{$key};
+            my $data ;
+
+            foreach my $func ( split( /,/ , $value ) )
+            {
+                $func =~ s/^\s+|\s+$//g;
+                next unless( length( $func ) );
+
+                push( @$data, { function => $func } );
+            }
+
+            #
+            # Make the look-variable "primitives" available to the
+            # page that invoked us.
+            #
+            $hash{ $key } = undef;
+            $hash{ $key } = $data if ( $data );
+        }
+    }
+
+    return ( \%hash );
+}
+
+
+#
+#  Register the plugin.
+#
+Templer::Plugin::Factory->new()->register_plugin("Templer::Plugin::seealso");
